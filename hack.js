a740f82af9e78cdd85f15369ed075b54763ad634
canuckistani <jeff@canuckistani.ca>
2013-02-23 15:14:19 -0800

    some hacking

---
+++ hack.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+var util = require('util'),
+    fs = require('fs'),
+    path = require('path'),
+    _ = require('underscore');
+
+var L = console.log,
+    D = function(o) { L(util.inspect(o)); },
+    F = function(s) { return util.format.apply(null, arguments); };
+
+
+
+var raw = fs.readFileSync('./data/scraped.txt', 'utf8');
+
+var routes = raw.split(/Route [\d]{3}/g);
+
+var test  = routes[1];
+
+// var pp = function(o) { return JSON.stringify(o,null,'  ')};
+
+var m = test.match(/Characteristics([\S\s]+?)?Productivity/g);
+
+L(m[0]);
+
+var lines = m[0].split("\n");
+
+L(lines.length);
+
+L(lines);
+
+
+
